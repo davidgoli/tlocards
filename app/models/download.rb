d5e7f22ed7edefc01cd587fbf4b3040efa6d09e4
David Golightly <david@substantial.com>
2012-02-11 21:49:17 -0800

    show count of redeemed codes

--- app/models/download.rb
+++ app/models/download.rb
@@ -3,4 +3,8 @@ class Download < ActiveRecord::Base
 
   has_attached_file :zipfile, :url => "/:class/:attachment/:id/:basename.:extension"
   attr_accessible :download, :artist, :title, :release_date
+
+  def redeemed_codes
+    download_codes.where(:redeemed => 1)
+  end
 end
--- app/models/download_code.rb
+++ app/models/download_code.rb
@@ -1,4 +1,5 @@
 class DownloadCode < ActiveRecord::Base
   belongs_to :download
   validates_uniqueness_of :code
+  attr_reader :redeemed?
 end
