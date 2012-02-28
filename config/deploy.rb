2659752ff938ffcd01926c0ff85183a41c2ab098
David Golightly <david@substantial.com>
2012-02-28 14:06:26 -0800

    deploy/env

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,23 @@
 require 'bundler/capistrano'
 set :application, "tlocards"
 set :repository,  "git@github.com:davidgoli/tlocards.git"
+set :user, "david"
+set :password, "pUkU8ad4"
+set :domain, "web213.webfaction.com"
+set :appdir, "webapps/tlocards/tlocards"
 
 set :scm, :git
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "redeem.translinguisticother.com"                          # Your HTTP server, Apache/etc
-role :app, "redeem.translinguisticother.com"                          # This may be the same as your `Web` server
-role :db,  "redeem.translinguisticother.com", :primary => true # This is where Rails migrations will run
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+set :deploy_to, appdir
+set :deploy_via, :export
 
 namespace :deploy do
   task :default do
