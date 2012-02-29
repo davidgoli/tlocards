dd49fb91a2364eeae9a2ec0505bf2770199d42de
David Golightly <david@substantial.com>
2012-02-28 19:08:55 -0800

    use rails 3.1 assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,7 @@ module Tlocards
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    config.assets.enabled = true
     config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
