07bf394e2a21655aa2cf48e82ed29b8646c291e8
David Golightly <davigoli@gmail.com>
2018-12-08 13:21:14 -0800

    make seeds save download codes; drop ie7 support

--- app/models/download_code_generator.rb
+++ app/models/download_code_generator.rb
@@ -6,7 +6,7 @@ class DownloadCodeGenerator
   def generate(count)
     target_count = @download.download_codes.length + count
     while @download.download_codes.length < target_count
-      @download.download_codes.build(:code => random_code)
+      @download.download_codes.build(:code => random_code).save!
     end
   end
 
